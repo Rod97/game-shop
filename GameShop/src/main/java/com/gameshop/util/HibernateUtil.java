f5743737c53a1c66fc724188e50c8e0d4a8f621a
Rodrigo <rflores456123@gmail.com>
2021-04-10 14:49:45 -0700

    added a simple console app that lets us add games to db

--- GameShop/src/main/java/com/gameshop/util/HibernateUtil.java
+++ GameShop/src/main/java/com/gameshop/util/HibernateUtil.java
@@ -1,6 +1,5 @@
 package com.gameshop.util;
 
-
 import java.util.Properties;
 
 import org.hibernate.SessionFactory;
@@ -9,35 +8,38 @@ import org.hibernate.cfg.Configuration;
 import org.hibernate.cfg.Environment;
 import org.hibernate.service.ServiceRegistry;
 
+import com.gameshop.model.Game;
 import com.gameshop.model.User;
 
 public class HibernateUtil {
-    private static SessionFactory sessionFactory;
-    public static SessionFactory getSessionFactory() {
-        if (sessionFactory == null) {
-            try {
-                Configuration configuration = new Configuration();
-                // Hibernate settings equivalent to hibernate.cfg.xml's properties
-                Properties settings = new Properties();
-                settings.put(Environment.DRIVER, "org.postgresql.Driver");
-                settings.put(Environment.URL, "jdbc:postgresql://" + System.getenv("gsdb_url") + ":5432/game_shop_db"
-                		+ "?currentSchema=shop_data");
-                settings.put(Environment.USER, "postgres");
-                settings.put(Environment.PASS, System.getenv("gsdb_password"));
-                settings.put(Environment.DIALECT, "org.hibernate.dialect.PostgreSQLDialect");
-                settings.put(Environment.SHOW_SQL, "true");
-               settings.put(Environment.CURRENT_SESSION_CONTEXT_CLASS, "thread");
-                settings.put(Environment.HBM2DDL_AUTO, "update");
-                configuration.setProperties(settings);
-                configuration.addAnnotatedClass(User.class);
-                
-                ServiceRegistry serviceRegistry = new StandardServiceRegistryBuilder()
-                    .applySettings(configuration.getProperties()).build();
-                sessionFactory = configuration.buildSessionFactory(serviceRegistry);
-            } catch (Exception e) {
-                e.printStackTrace();
-            }
-        }
-        return sessionFactory;
-    }
+	private static SessionFactory sessionFactory;
+
+	public static SessionFactory getSessionFactory() {
+		if (sessionFactory == null) {
+			try {
+				Configuration configuration = new Configuration();
+				// Hibernate settings equivalent to hibernate.cfg.xml's properties
+				Properties settings = new Properties();
+				settings.put(Environment.DRIVER, "org.postgresql.Driver");
+				settings.put(Environment.URL, "jdbc:postgresql://" + System.getenv("gsdb_url") + ":5432/game_shop_db"
+						+ "?currentSchema=shop_data");
+				settings.put(Environment.USER, "postgres");
+				settings.put(Environment.PASS, System.getenv("gsdb_password"));
+				settings.put(Environment.DIALECT, "org.hibernate.dialect.PostgreSQLDialect");
+				settings.put(Environment.SHOW_SQL, "true");
+				settings.put(Environment.CURRENT_SESSION_CONTEXT_CLASS, "thread");
+				settings.put(Environment.HBM2DDL_AUTO, "update");
+				configuration.setProperties(settings);
+				configuration.addAnnotatedClass(User.class);
+				configuration.addAnnotatedClass(Game.class);
+
+				ServiceRegistry serviceRegistry = new StandardServiceRegistryBuilder()
+						.applySettings(configuration.getProperties()).build();
+				sessionFactory = configuration.buildSessionFactory(serviceRegistry);
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+		}
+		return sessionFactory;
+	}
 }
