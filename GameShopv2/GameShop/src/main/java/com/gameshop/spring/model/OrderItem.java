d19e0cd28ba1784be668e102320311f03e344398
Max Nicolai <maxvnicolai@gmail.com>
2021-04-19 10:09:35 -0700

    OrderItem now has real foreign key mappings

--- GameShopv2/GameShop/src/main/java/com/gameshop/spring/model/OrderItem.java
+++ GameShopv2/GameShop/src/main/java/com/gameshop/spring/model/OrderItem.java
@@ -2,10 +2,16 @@ package com.gameshop.spring.model;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
 import javax.persistence.Table;
+
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonProperty;
 
 import lombok.Data;
 
@@ -16,27 +22,35 @@ public @Data class OrderItem {
 	@Id
 	@GeneratedValue(strategy = GenerationType.AUTO)
 	private Long id;
-
-	/*
-	 * These worked well for creating association but were awful to work with
-	 * Leaving them in case I have time to go back over this
-	 * 
-	 * @ManyToOne(fetch=FetchType.LAZY)
-	 * 
-	 * @JoinColumn(name="invoice_number") private Order order;
-	 * 
-	 * @ManyToOne(fetch=FetchType.LAZY)
-	 * 
-	 * @JoinColumn(name="upc") private Items item;
-	 */
-
-	// Invoice num for order
-	@Column(name = "order_number")
-	private long order;
-	// Upc of item
-	@Column(name = "item_upc")
-	private long item;
-	// Number of item ordered
-	@Column(name = "quanity")
-	private int quantity;
+	
+	 @ManyToOne(fetch=FetchType.LAZY)
+	@JoinColumn(name="invoice_number") 
+	 private Order order;
+	 
+	 @ManyToOne(fetch=FetchType.LAZY)
+	 @JoinColumn(name="upc") 
+	 private Items item;
+	 
+	 @JsonCreator
+	 public OrderItem(@JsonProperty("order") Long order, @JsonProperty("item") Long item, @JsonProperty("quantity") int quantity) {
+		 this.order = new Order();
+		 this.order.setInvoiceNumber(order);
+		 this.item = new Items();
+		 this.item.setUpc(item);
+		 this.quantity = quantity;
+	 }
+	 
+	 @Column(name = "quanity")
+	 private int quantity;
+	 
+	 //Default constructor
+	 public OrderItem() {};
+	 //	// Invoice num for order
+//	//@Column(name = "order_number")
+//	@JSONProperty("order")
+//	private long order;
+//	// Upc of item
+//	@Column(name = "item_upc")
+//	private long item;
+// Number of item ordered
 }
