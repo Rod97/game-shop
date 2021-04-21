26e13c2a94e079b485388db524d45dc34665a047
Max Nicolai <maxvnicolai@gmail.com>
2021-04-21 10:05:25 -0700

    search

--- Angular/Gameshop/src/app/storefront/storefront.component.ts
+++ Angular/Gameshop/src/app/storefront/storefront.component.ts
@@ -1,90 +1,101 @@
 import { Component, OnInit } from '@angular/core';
 import { Game } from '../models/Game';
-import { StorefrontService } from './storefront.service'
+import { StorefrontService } from './storefront.service';
 
 @Component({
   selector: 'app-storefront',
   templateUrl: './storefront.component.html',
-  styleUrls: ['./storefront.component.css']
+  styleUrls: ['./storefront.component.css'],
 })
 export class StorefrontComponent implements OnInit {
   title = 'json-file-read-angular';
   private game: Game;
-  
 
-  view = "all";
-
-
-  constructor(private storefrontService: StorefrontService) { }
+  view = 'all';
+  interval: any;
+  constructor(private storefrontService: StorefrontService) {}
 
   ngOnInit(): void {
     if (this.storefrontService.allGames.length == 0) {
-    this.storefrontService.GetGames().subscribe(data => {
-      for (let gameData in data) {
-        this.game = data[gameData];
-        this.game.image = this.formatImage(this.game.image);
-        this.storefrontService.allGames.push(this.game);
+      this.storefrontService.GetGames().subscribe((data) => {
+        for (let gameData in data) {
+          this.game = data[gameData];
+          this.game.image = this.formatImage(this.game.image);
+          this.storefrontService.allGames.push(this.game);
+        }
+      });
+    }
+    this.view = 'all';
+    this.interval = setInterval(() => this.rs(), 100);
+  }
 
-      }
-  });
-  
-}
-this.view = 'all';
-}
+  formatImage(img: any): any {
+    return 'data:image/jpeg;base64,' + img;
+  }
 
+  all() {
+    this.view = 'all';
+  }
 
-
-formatImage(img: any): any {
-  return 'data:image/jpeg;base64,' + img;
-}
-
-all() {
-  this.view = "all";
-}
-
-ps2() {
-  if (this.storefrontService.ps2Games.length == 0) {
-    for (let i = 0; i < this.storefrontService.allGames.length; i++) {
-      if (this.storefrontService.allGames[i].platform === "PS2") {
-        this.storefrontService.ps2Games.push(this.storefrontService.allGames[i]);
+  ps2() {
+    if (this.storefrontService.ps2Games.length == 0) {
+      for (let i = 0; i < this.storefrontService.allGames.length; i++) {
+        if (this.storefrontService.allGames[i].platform === 'PS2') {
+          this.storefrontService.ps2Games.push(
+            this.storefrontService.allGames[i]
+          );
+        }
       }
     }
+    this.view = 'ps2';
   }
-  this.view = "ps2";
-}
 
-gc() {
-  if (this.storefrontService.gcGames.length == 0) {
-    for (let i = 0; i < this.storefrontService.allGames.length; i++) {
-      if (this.storefrontService.allGames[i].platform === "GameCube") {
-        this.storefrontService.gcGames.push(this.storefrontService.allGames[i]);
+  gc() {
+    if (this.storefrontService.gcGames.length == 0) {
+      for (let i = 0; i < this.storefrontService.allGames.length; i++) {
+        if (this.storefrontService.allGames[i].platform === 'GameCube') {
+          this.storefrontService.gcGames.push(
+            this.storefrontService.allGames[i]
+          );
+        }
       }
     }
+    this.view = 'gc';
   }
-  this.view = "gc";
-}
 
-xb() {
-  if (this.storefrontService.xbGames.length == 0) {
-    for (let i = 0; i < this.storefrontService.allGames.length; i++) {
-      if (this.storefrontService.allGames[i].platform === "Xbox") {
-        this.storefrontService.xbGames.push(this.storefrontService.allGames[i]);
+  xb() {
+    if (this.storefrontService.xbGames.length == 0) {
+      for (let i = 0; i < this.storefrontService.allGames.length; i++) {
+        if (this.storefrontService.allGames[i].platform === 'Xbox') {
+          this.storefrontService.xbGames.push(
+            this.storefrontService.allGames[i]
+          );
+        }
       }
     }
+    this.view = 'xb';
   }
-  this.view = "xb";
-}
 
-dc() {
-  if (this.storefrontService.dcGames.length == 0) {
-    for (let i = 0; i < this.storefrontService.allGames.length; i++) {
-      if (this.storefrontService.allGames[i].platform === "Dreamcast") {
-        this.storefrontService.dcGames.push(this.storefrontService.allGames[i]);
+  dc() {
+    if (this.storefrontService.dcGames.length == 0) {
+      for (let i = 0; i < this.storefrontService.allGames.length; i++) {
+        if (this.storefrontService.allGames[i].platform === 'Dreamcast') {
+          this.storefrontService.dcGames.push(
+            this.storefrontService.allGames[i]
+          );
+        }
       }
     }
+    this.view = 'dc';
   }
-  this.view = "dc";
-}
 
-
+  rs() {
+    if (this.storefrontService.rsGames.length != 0) {
+      if (this.view != 'rs') {
+        this.view = 'rs';
+      }
+    } else {
+      this.view = 'all';
+    }
+  }
 }
--- Angular/Gameshop/src/app/storefront/storefront.service.ts
+++ Angular/Gameshop/src/app/storefront/storefront.service.ts
@@ -5,31 +5,42 @@ import { Observable, throwError } from 'rxjs';
 import { Game } from '../models/Game';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class StorefrontService {
-
   public allGames: Game[] = [];
   public ps2Games: Game[] = [];
   public gcGames: Game[] = [];
   public xbGames: Game[] = [];
   public dcGames: Game[] = [];
+  public rsGames: Game[] = [];
 
-  baseurl = 'http://gsdb-env.eba-fv8dzysz.us-east-2.elasticbeanstalk.com/items/games/'
+  baseurl =
+    'http://gsdb-env.eba-fv8dzysz.us-east-2.elasticbeanstalk.com/items/games/';
 
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient) {}
   httpOptions = {
     headers: new HttpHeaders({
-      'Content-Type':'application/json'
-    })
+      'Content-Type': 'application/json',
+    }),
+  };
+  GetGames(): Observable<Game> {
+    return this.http.get<Game>(`${this.baseurl}`);
   }
-  GetGames():Observable<Game>{
-    return this.http.get<Game>(`${this.baseurl}`)
-    
- }
- GetGameByUpc(id: string):Observable<Game>{
-  return this.http.get<Game>(`${this.baseurl}${id}`)
-  
-}
-    
+  GetGameByUpc(id: string): Observable<Game> {
+    return this.http.get<Game>(`${this.baseurl}${id}`);
+  }
+  searchGames(search: string) {
+    if (search.length >= 1) {
+      this.rsGames = this.allGames.filter((Game) => {
+        if (Game.itemName.includes(search)) {
+          return Game;
+        } else {
+          return false;
+        }
+      });
+    } else {
+      this.rsGames = [];
+    }
+  }
 }
