1ef26ba59ad266b1675a7124bbea8184ccd2f12d
Rod97 <53624506+Rod97@users.noreply.github.com>
2021-04-28 08:49:06 -0700

    Delete GameShopv2/GameShop directory

--- GameShopv2/GameShop/src/main/java/com/gameshop/spring/repository/OrderItemRepository.java
+++
@@ -1,9 +0,0 @@
-package com.gameshop.spring.repository;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.gameshop.spring.model.OrderItem;
-
-
-public interface OrderItemRepository extends JpaRepository<OrderItem, Long>{
-
-}
--- GameShopv2/GameShop/src/main/java/com/gameshop/spring/repository/OrderRepository.java
+++
@@ -1,9 +0,0 @@
-package com.gameshop.spring.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.gameshop.spring.model.Order;
-
-public interface OrderRepository extends JpaRepository<Order, Long> {
-
-}
